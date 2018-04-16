460c7e47e14c9203d8a27531d33404269ab7d6cc
zap-ynonaka <y_nonaka@zappallas.com>
2018-04-16 18:56:41 +0900

    0416fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,185 +4,273 @@
 <link rel="stylesheet" href="/css/app.css">
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <body>
-<header>このへんヘッダー</header>
+<header class="header">
+  <div><a href="#"><img src="https://placehold.jp/26/3d4070/ffffff/100x100.png" alt=""></a></div>
+  <div><a href="/"><h1><img src="https://placehold.jp/26/3d4070/ffffff/220x68.png" alt="３０〜５０文字前後で説明"></h1></a></div>
+  <div><a href="/user/mypage"><img src="https://placehold.jp/26/3d4070/ffffff/100x100.png" alt="マイページ"></a></div>
+</header>
 <main>
 <section>
-  <h2 class="title-base">3月20日のおすすめ</h2>
-  <ul class="list-article__base">
-    <li class="pickup">
-      <a href="#">
-        <div><img src="http://placehold.jp/375x200.png" alt=""></div>
-        <p>他人への感謝の気持ち、どうやって育ててあげればいい？a</p>
-      </a>
-    </li>
-    <li class="">
-      <a href="#">
-        <div>
-          <img src="http://placehold.jp/375x200.png" alt="">
-        </div>
-        <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
-      </a>
-    </li>
-    <li class="">
-      <a href="#">
-        <div>
-          <img src="http://placehold.jp/375x200.png" alt="">
-        </div>
-        <p>本当はもっとニコニコなママでいたい、どうしたらいいの？</p>
-      </a>
-    </li>
-  </ul>
-</section>
-
-<section>
-  <h2 class="title-base">今日の運勢 3月20日</h2>
-  <ul class="js-tab-list tab-list">
-    <li class="js-active">総合運</li>
-    <li>家庭運</li>
-    <li>金運</li>
-  </ul>
-  <ul class="js-tab-area tab-content">
-    <li class="js-active">
-      <div class="tab-content__result">
-        難しい選択を迫られそうです。返事を待ってもらうこともできますが、早く前に進みたいと思う場合は、自分の胸に聞いて決断してください。きっと自由を手に入れられます。また、これまで周りにいなかったタイプの人と縁がある日です。男女、年齢、職業などを超えて、同じ価値観で結ばれた仲間としてのおつき合いが始まりそう。さまざまな可能性があって、思いのままに生きることの素晴らしさを、実際の行動を通じて教えてもらえるでしょう。
-      </div>
-      <div class="tab-content__recommend">
-        <h3 class="title-base">今日のラッキーフォト</h3>
-        <p>子供のパジャマすたがの写真</p>
-      </div>
-      <div class="button-base">
-        <a href="#">今週の運勢を見る</a>
-      </div>
-    </li>
-    
-    <li>
-      <div>
-        難しい選択を迫られそうです。返事を待ってもらうこともできますが、早く前に進みたいと思う場合は、自分の胸に聞いて決断してください。きっと自由を手に入れられます。また、これまで周りにいなかったタイプの人と縁がある日です。男女、年齢、職業などを超えて、同じ価値観で結ばれた仲間としてのおつき合いが始まりそう。さまざまな可能性があって、思いのままに生きることの素晴らしさを、実際の行動を通じて教えてもらえるでしょう。
-      </div>
-      <div>
-        <h3>今日のラッキーフォト</h3>
-        <p>子供のパジャマすたがの写真</p>
-      </div>
-      <div>
-        <a href="#">今週の運勢を見る</a>
-      </div>
-    </li>
-
-    <li>
-      <div>
-        難しい選択を迫られそうです。返事を待ってもらうこともできますが、早く前に進みたいと思う場合は、自分の胸に聞いて決断してください。きっと自由を手に入れられます。また、これまで周りにいなかったタイプの人と縁がある日です。男女、年齢、職業などを超えて、同じ価値観で結ばれた仲間としてのおつき合いが始まりそう。さまざまな可能性があって、思いのままに生きることの素晴らしさを、実際の行動を通じて教えてもらえるでしょう。
-      </div>
-      <div>
-        <h3>今日のラッキーフォト</h3>
-        <p>子供のパジャマすたがの写真</p>
-      </div>
-      <div>
-        <a href="#">今週の運勢を見る</a>
-      </div>
-    </li>
-  </ul>
-</section>
-
-
-
-<section>
-  <h2 class="title-base">これって大丈夫？</h2>
-  <ul class="list-recommend__base">
-    <li class="">
-      <a href="#">
-        <div>
-          <img src="http://placehold.jp/375x200.png" alt="">
-        </div>
+  <h2 class="title-base"><div><span>3月20日<br>今月のおすすめ</span></span></h2>
+  <div class="content-space__top">
+    <ul class="list-article__base">
+      <li class="pickup">
+        <a href="#">
+          <div><img src="http://placehold.jp/375x200.png" alt=""></div>
+          <p>他人への感謝の気持ち、どうやって育ててあげればいい？a</p>
+        </a>
+      </li>
+      <li class="">
+        <a href="#">
+          <div>
+            <img src="http://placehold.jp/375x200.png" alt="">
+          </div>
           <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
-      </a>
-    </li>
-    <li class="">
-      <a href="#">
-      <div>
-        <img src="http://placehold.jp/375x200.png" alt="">
-      </div>
-      <p>本当はもっとニコニコなママでいたい、どうしたらいいの？</p>
-      </a>
-    </li>
-    <li class="">
-      <a href="#">
-      <div>
-        <img src="http://placehold.jp/375x200.png" alt="">
-      </div>
-      <p>本当はもっとニコニコなママでいたい、どうしたらいいの？</p>
-      </a>
-    </li>
-    <li class="">
-      <a href="#">
-      <div>
-        <img src="http://placehold.jp/375x200.png" alt="">
-      </div>
-      <p>本当はもっとニコニコなママでいたい、どうしたらいいの？</p>
-      </a>
-    </li>
-    <li class="">
-      <a href="#">
-      <div>
-        <img src="http://placehold.jp/375x200.png" alt="">
-      </div>
-      <p>本当はもっとニコニコなママでいたい、どうしたらいいの？</p>
-      </a>
-    </li>
-  </ul>
-
-  <div class="button-base">
-    <button>もっと見る</button>
+        </a>
+      </li>
+      <li class="">
+        <a href="#">
+          <div>
+            <img src="http://placehold.jp/375x200.png" alt="">
+          </div>
+          <p>本当はもっとニコニコなママでいたい、どうしたらいいの？</p>
+        </a>
+      </li>
+    </ul>
   </div>
 </section>
 
 <section>
-  <h2 class="title-base">私っていいママ？</h2>
-  <ul class="list-article__base">
-    <li class="">
-      <a href="#">
-        <div>
-          <img src="http://placehold.jp/375x200.png" alt="">
+  <h2 class="title-base"><div><span>毎日チェック<br>4月16日の運勢</span></span></h2>
+  <div class="content-space__top">
+    <ul class="js-tab-list tab-list">
+      <li class="js-active"><span>総合運</span></li>
+      <li><span>家庭運</span></li>
+      <li><span>金運</span></li>
+    </ul>
+    <ul class="js-tab-area tab-content">
+      <li class="js-active">
+        <div class="tab-content__result">
+          難しい選択を迫られそうです。返事を待ってもらうこともできますが、早く前に進みたいと思う場合は、自分の胸に聞いて決断してください。きっと自由を手に入れられます。また、これまで周りにいなかったタイプの人と縁がある日です。男女、年齢、職業などを超えて、同じ価値観で結ばれた仲間としてのおつき合いが始まりそう。さまざまな可能性があって、思いのままに生きることの素晴らしさを、実際の行動を通じて教えてもらえるでしょう。
         </div>
-        <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
-      </a>
-    </li>
-    <li class="">
-      <a href="#">
-        <div>
-          <img src="http://placehold.jp/375x200.png" alt="">
+        <div class="tab-content__recommend">
+          <h3 class="title-base">今日のラッキーフォト</h3>
+          <p>子供のパジャマすたがの写真</p>
         </div>
-        <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
-      </a>
-    </li>
-    <li class="">
-      <a href="#">
-        <div>
-          <img src="http://placehold.jp/375x200.png" alt="">
+        <div class="button-base">
+          <a href="#">今週の運勢を見る</a>
         </div>
-        <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
-      </a>
-    </li>
-    <li class="">
-      <a href="#">
+      </li>
+      
+      <li>
         <div>
-          <img src="http://placehold.jp/375x200.png" alt="">
+          難しい選択を迫られそうです。返事を待ってもらうこともできますが、早く前に進みたいと思う場合は、自分の胸に聞いて決断してください。きっと自由を手に入れられます。また、これまで周りにいなかったタイプの人と縁がある日です。男女、年齢、職業などを超えて、同じ価値観で結ばれた仲間としてのおつき合いが始まりそう。さまざまな可能性があって、思いのままに生きることの素晴らしさを、実際の行動を通じて教えてもらえるでしょう。
         </div>
-        <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
-      </a>
-    </li>
-    <li class="">
-      <a href="#">
         <div>
-          <img src="http://placehold.jp/375x200.png" alt="">
+          <h3>今日のラッキーフォト</h3>
+          <p>子供のパジャマすたがの写真</p>
         </div>
-        <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
-      </a>
-    </li>
-  </ul>
+        <div>
+          <a href="#">今週の運勢を見る</a>
+        </div>
+      </li>
 
+      <li>
+        <div>
+          難しい選択を迫られそうです。返事を待ってもらうこともできますが、早く前に進みたいと思う場合は、自分の胸に聞いて決断してください。きっと自由を手に入れられます。また、これまで周りにいなかったタイプの人と縁がある日です。男女、年齢、職業などを超えて、同じ価値観で結ばれた仲間としてのおつき合いが始まりそう。さまざまな可能性があって、思いのままに生きることの素晴らしさを、実際の行動を通じて教えてもらえるでしょう。
+        </div>
+        <div>
+          <h3>今日のラッキーフォト</h3>
+          <p>子供のパジャマすたがの写真</p>
+        </div>
+        <div>
+          <a href="#">今週の運勢を見る</a>
+        </div>
+      </li>
+    </ul>
+  </div>
 </section>
 
+
+<section>
+  <h2 class="title-base"><div><span>いつでも知りたい<br>Kid's ごきげん予報</span></span></h2>
+  <div class="content-space__top">
+    ここにコンテンツー！
+  </div>
+</section>
+
+
+<section>
+  <h2 class="title-base"><div><span>もしかして私だけ？<br>これって大丈夫？</span></span></h2>
+  <div class="content-space__top">
+    <ul class="list-recommend__base">
+      <li class="">
+        <a href="#">
+          <div>
+            <img src="http://placehold.jp/375x200.png" alt="">
+          </div>
+            <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
+        </a>
+      </li>
+      <li class="">
+        <a href="#">
+        <div>
+          <img src="http://placehold.jp/375x200.png" alt="">
+        </div>
+        <p>本当はもっとニコニコなママでいたい、どうしたらいいの？</p>
+        </a>
+      </li>
+      <li class="">
+        <a href="#">
+        <div>
+          <img src="http://placehold.jp/375x200.png" alt="">
+        </div>
+        <p>本当はもっとニコニコなママでいたい、どうしたらいいの？</p>
+        </a>
+      </li>
+      <li class="">
+        <a href="#">
+        <div>
+          <img src="http://placehold.jp/375x200.png" alt="">
+        </div>
+        <p>本当はもっとニコニコなママでいたい、どうしたらいいの？</p>
+        </a>
+      </li>
+      <li class="">
+        <a href="#">
+        <div>
+          <img src="http://placehold.jp/375x200.png" alt="">
+        </div>
+        <p>本当はもっとニコニコなママでいたい、どうしたらいいの？</p>
+        </a>
+      </li>
+    </ul>
+
+    <div class="button-base">
+      <button>もっと見る</button>
+    </div>
+  </div>
+</section>
+
+<section>
+  <h2 class="title-base"><div><span>ああああああああ<br>私っていいママ？</span></span></h2>
+  <div class="content-space__top">
+    <ul class="list-article__base">
+      <li class="">
+        <a href="#">
+          <div>
+            <img src="http://placehold.jp/375x200.png" alt="">
+          </div>
+          <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
+        </a>
+      </li>
+      <li class="">
+        <a href="#">
+          <div>
+            <img src="http://placehold.jp/375x200.png" alt="">
+          </div>
+          <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
+        </a>
+      </li>
+      <li class="">
+        <a href="#">
+          <div>
+            <img src="http://placehold.jp/375x200.png" alt="">
+          </div>
+          <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
+        </a>
+      </li>
+      <li class="">
+        <a href="#">
+          <div>
+            <img src="http://placehold.jp/375x200.png" alt="">
+          </div>
+          <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
+        </a>
+      </li>
+      <li class="">
+        <a href="#">
+          <div>
+            <img src="http://placehold.jp/375x200.png" alt="">
+          </div>
+          <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
+        </a>
+      </li>
+    </ul>
+  </div>
+</section>
+
+
+<section>
+  <h2 class="title-base"><div><span>いつでも知りたい<br>Kid's ごきげん予報</span></span></h2>
+  <div class="content-space__top">
+    ここにコンテンツー！
+  </div>
+</section>
+
+
+<section>
+  <h2 class="title-base"><div><span>ああああああああ<br>相性診断</span></span></h2>
+  <div class="content-space__top">
+    <ul class="list-article__base">
+      <li class="">
+        <a href="#">
+          <div>
+            <img src="http://placehold.jp/375x200.png" alt="">
+          </div>
+          <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
+        </a>
+      </li>
+      <li class="">
+        <a href="#">
+          <div>
+            <img src="http://placehold.jp/375x200.png" alt="">
+          </div>
+          <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
+        </a>
+      </li>
+      <li class="">
+        <a href="#">
+          <div>
+            <img src="http://placehold.jp/375x200.png" alt="">
+          </div>
+          <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
+        </a>
+      </li>
+      <li class="">
+        <a href="#">
+          <div>
+            <img src="http://placehold.jp/375x200.png" alt="">
+          </div>
+          <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
+        </a>
+      </li>
+      <li class="">
+        <a href="#">
+          <div>
+            <img src="http://placehold.jp/375x200.png" alt="">
+          </div>
+          <p>他人への感謝の気持ち、どうやって育ててあげればいい？</p>
+        </a>
+      </li>
+    </ul>
+  </div>
+</section>
+
+
 </main>
+
+<footer class="footer">
+  <ul>
+    <li><a href="/help/terms">利用規約</a></li>
+    <li><a href="#">特定商法に関する表記</a></li>
+    <li><a href="#">ヘルプ＆サポート</a></li>
+    <li><a href="#">Cookieの利用について</a></li>
+    <li><a href="/help/privacy">プライバシーポリシー</a></li>
+    <li><a href="#">運営会社</a></li>
+  </ul>
+  <div class="footer-copy">ZAPPALLAS, Inc.</div>
+</footer>
 <br>
 <br>
 <!-- 未作成
