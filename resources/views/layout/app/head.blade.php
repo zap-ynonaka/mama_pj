8ffcd011ac64bb7f0ff59c24b7bfc8083a7ff302
zap-ynonaka <y_nonaka@zappallas.com>
2018-04-18 16:17:49 +0900

    mypagelist fix

--- resources/views/layout/app/head.blade.php
+++ resources/views/layout/app/head.blade.php
@@ -2,6 +2,8 @@
 <meta name="author" content="ZAPPALLAS, Inc.">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
+<meta name="viewport" content="width=device-width,initial-scale=1">
+
 {{-- 会員登録用 css & js --}}
 <link rel="stylesheet" type="text/css" href="/css/plus-custom.css">
 <link rel="stylesheet" type="text/css" href="/css/plus-style.css">
